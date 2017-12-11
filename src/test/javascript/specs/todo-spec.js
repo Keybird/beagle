3f509884c499155b9f517286dd5410a292f91eb1
Markus von Rüden <mvr@opennms.com>
2017-12-11 18:56:45 +0100

    Add e2e test possibilities via protractor

---
+++ src/test/javascript/specs/todo-spec.js
@@ -0,0 +1,37 @@
+/*
+ * This file is part of Beagle.
+ * Copyright (c) 2017 Markus von Rüden.
+ *
+ * Beagle is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Beagle is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+ * the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Beagle. If not, see http://www.gnu.org/licenses/.
+ */
+
+'use strict';
+
+describe('angularjs homepage todo list', function() {
+    it('should add a todo', function() {
+        browser.get('https://angularjs.org');
+
+        element(by.model('todoList.todoText')).sendKeys('write first protractor test');
+        element(by.css('[value="add"]')).click();
+
+        var todoList = element.all(by.repeater('todo in todoList.todos'));
+        expect(todoList.count()).toEqual(3);
+        expect(todoList.get(2).getText()).toEqual('write first protractor test');
+
+        // You wrote your first test, cross it off the list
+        todoList.get(2).element(by.css('input')).click();
+        var completedAmount = element.all(by.css('.done-true'));
+        expect(completedAmount.count()).toEqual(2);
+    });
+});
